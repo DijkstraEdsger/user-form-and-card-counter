a054450f19ad9a8ec3f632e06e2b614789be3372
humbertomartinez900519 <humbertomartinez900519@gmail.com>
2020-11-03 15:23:59 -0500

    routes navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { CountCompleteCardsComponent } from './components/count-complete-cards/count-complete-cards.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-form',
+    component: CreateUserComponent,
+  },
+  {
+    path: 'complete-cards',
+    component: CountCompleteCardsComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'user-form',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
