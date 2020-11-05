1f368f42de02a36a5c4f627c3d1a503be2ca1d1a
humbertomartinez900519 <humbertomartinez900519@gmail.com>
2020-11-05 17:42:57 -0500

    unit tests in card service

--- src/app/services/card/card.service.spec.ts
+++ src/app/services/card/card.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CardService } from './card.service';
+import { Card } from '../../classes/card';
 
 describe('CardService', () => {
   let service: CardService;
@@ -12,5 +13,28 @@ describe('CardService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return cards list synchronously', () => {
+    const cards: Card[] = [
+      {
+        suit: 'hearts',
+        value: 2,
+      },
+      {
+        suit: 'clubs',
+        value: 9,
+      },
+      {
+        suit: 'diamonds',
+        value: 'J',
+      },
+      {
+        suit: 'spades',
+        value: 'A',
+      },
+    ];
+    service.cards = cards;
+    expect(service.getAllCards()).toEqual(cards);
   });
 });
