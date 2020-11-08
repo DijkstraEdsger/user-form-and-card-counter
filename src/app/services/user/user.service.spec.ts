ed638e4460273205460ffe944ea7a8d6765ae229
humbertomartinez900519 <humbertomartinez900519@gmail.com>
2020-11-08 04:24:54 -0500

    working in unit test for userService

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -2,57 +2,52 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { User } from '../../classes/user';
+import { defer } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    userService = new UserService(httpClientSpy as any);
+
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
   });
 
-  it('getUsers should return users list from observable', (done: DoneFn) => {
+  it('should return expected users (HttpClient called once)', () => {
     const expectedUsers: User[] = [
-      new User('John', 'Doe', null, null, null, '1'),
-      new User('Maria', 'Zalas', 25, 'maria@email', 'Holguin, Cuba', '2'),
+      {
+        id: '1',
+        name: 'Maria',
+        lastName: 'Lopez',
+      },
+      {
+        id: '2',
+        name: 'John',
+        lastName: 'Doe',
+      },
     ];
-    service.users = expectedUsers;
-    service.getUsers().subscribe((users) => {
-      expect(users).toEqual(expectedUsers, 'expected users');
-      done();
-    });
-  });
 
-  it('getUser should return an user from observable', (done: DoneFn) => {
-    const users: User[] = [
-      new User('John', 'Doe', null, null, null, '1'),
-      new User('Maria', 'Zalas', 25, 'maria@email', 'Holguin, Cuba', '2'),
-    ];
-    service.users = users;
-    service.getUser(users[0].id).subscribe((user) => {
-      expect(user).toEqual(users[0], 'expected users');
-      done();
-    });
-  });
+    httpClientSpy.get.and.returnValue(asyncData(expectedUsers));
 
-  it('createUser should create an user from observable', (done: DoneFn) => {
-    const users: User[] = [new User('John', 'Doe', null, null, null, '1')];
-    const newUserData: User = new User(
-      'Maria',
-      'Zalas',
-      25,
-      'maria@email',
-      'Holguin, Cuba',
-      '2'
-    );
-    service.users = users;
-    service.createUser(newUserData).subscribe((user) => {
-      expect(users).toContain(users[0], 'user created');
-      done();
-    });
+    userService
+      .getUsers()
+      .subscribe(
+        (response) =>
+          expect(response.data).toEqual(expectedUsers, 'expected users'),
+        fail
+      );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
+
+export function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
